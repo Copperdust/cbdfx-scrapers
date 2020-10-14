33abd034aac36d74688ba2fd4c2ba8081cbc80ab
Fabio Lucas Pampin <fabio.pampin@gm2dev.com>
2020-10-14 17:49:20 -0300

    Improve separation and reduce nesting

--- includes/base.js
+++ includes/base.js
@@ -20,41 +20,36 @@ class BaseScrapeClass {
     return axios(axiosConfig);
   }
 
-  async inspectPageForItems(url, acc, recurseForMoreItems = true) {
-    // Make a request for a user with a given ID
-    return new Promise((resolve, reject) => {
-      this.limiter.removeTokens(1, () => {
-        this.axiosRequest(url)
-          .then(async response => {
-            // Send message to inform a request was made
-            console.log("Did a request to " + url);
-            // Parse response
-            const document = this.getDocument(response);
-            // Accumulate comment
-            this.parseForItems(document, acc, url);
-            // Check for more pages
-            if (recurseForMoreItems) {
-              await this.recurseParseForMoreItems(document, acc, url, this.inspectPageForItems.bind(this));
-            }
-            resolve();
-          })
-          .catch(function (error) {
-            // Retry (currently forever!!!) but only subpages
+  async processSuccessfulRequest(acc, url, response, recurseForMoreItems) {
+    // Send message to inform a request was made
+    console.log("Did a request to " + url);
+    // Parse response
+    const document = this.getDocument(response);
+    // Accumulate comment
+    this.parseForItems(document, acc, url);
+    // Check for more pages
+    if (recurseForMoreItems) {
+      await this.recurseParseForMoreItems(document, acc, url, this.inspectPageForItems.bind(this));
+    }
+  }
+
+  inspectPageForItems(url, acc, recurseForMoreItems = true) {
+    try {
+      return new Promise((resolve, reject) => {
+        this.limiter.removeTokens(1, async () => {
+          try {
+            const response = await this.axiosRequest.bind(this)(url);
+            await this.processSuccessfulRequest(acc, url, response, recurseForMoreItems);
+          } catch (error) {
             if (!recurseForMoreItems) {
               this.inspectPageForItems(url, acc, recurseForMoreItems);
             }
-            // handle error
-            reject(error);
-          });
+          }
+        });
       });
-    })
-      .catch(error => {
-        try {
-          console.log(url, error.response.statusText);
-        } catch (e2) {
-          console.log(url, error);
-        }
-      });
+    } catch (error) {
+      console.log(url, error.response ? error.response.statusText : error);
+    }
   };
 
   async asyncForEach(array, callback) {
