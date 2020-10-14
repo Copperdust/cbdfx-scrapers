c584e7bb742b1181987db6431090f990e368d05a
Fabio Lucas Pampin <fabio.pampin@gm2dev.com>
2020-10-14 17:32:28 -0300

    Move main 'inspectPageForItems' function inside a base class

--- includes/base.js
+++ includes/base.js
@@ -1,6 +1,16 @@
 const axios = require('axios');
+const RateLimiter = require('limiter').RateLimiter;
 
 class BaseScrapeClass {
+  limiterConfig = {
+    amount: 5,
+    timespan: 'second'
+  }
+  
+  constructor() {
+    this.limiter = new RateLimiter(this.limiterConfig.amount, this.limiterConfig.timespan);
+  }
+
   axiosRequest(url) {
     const axiosConfig = {
       url: url,
@@ -9,6 +19,43 @@ class BaseScrapeClass {
     }
     return axios(axiosConfig);
   }
+
+  async inspectPageForItems(url, acc, recurseForMoreItems = true) {
+    // Make a request for a user with a given ID
+    return new Promise((resolve, reject) => {
+      this.limiter.removeTokens(1, async () => {
+        await this.axiosRequest(url)
+          .then(async response => {
+            // Send message to inform a request was made
+            console.log("Did a request to " + url);
+            // Parse response
+            const document = this.getDocument(response);
+            // Accumulate comment
+            this.parseForItems(document, acc, url);
+            // Check for more pages
+            if (recurseForMoreItems) {
+              await this.recurseParseForMoreItems(document, acc, url, this.inspectPageForItems.bind(this));
+            }
+            resolve();
+          })
+          .catch(function (error) {
+            // Retry (currently forever!!!) but only subpages
+            if (!recurseForMoreItems) {
+              this.inspectPageForItems(url, acc, recurseForMoreItems);
+            }
+            // handle error
+            reject(error);
+          });
+      });
+    })
+      .catch(error => {
+        try {
+          console.log(url, error.response.statusText);
+        } catch (e2) {
+          console.log(url, error);
+        }
+      });
+  };
 }
 
 module.exports = BaseScrapeClass;
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const axios = require('axios');
 const ObjectsToCsv = require('objects-to-csv');
 const readlineSync = require('readline-sync');
 const fs = require('fs');
-
-// Rate Limiter
-const RateLimiter = require('limiter').RateLimiter;
-var limiter = new RateLimiter(5, 'second');
 
 // Get config to use
 const scrapeConfigs = fs.readdirSync('./models/').map(str => str.replace(/\.js$/, ''));
@@ -13,43 +8,6 @@ const scrapeConfig = scrapeConfigs[readlineSync.keyInSelect(scrapeConfigs, 'What
 
 // Init conifg
 const config = require('./models/' + scrapeConfig);
-
-const inspectPageForItems = async (url, acc, recurseForMoreItems = true) => {
-  // Make a request for a user with a given ID
-  return new Promise((resolve, reject) => {
-    limiter.removeTokens(1, async () => {
-      await config.axiosRequest(url)
-        .then(async function (response) {
-          // Send message to inform a request was made
-          console.log("Did a request to " + url);
-          // Parse response
-          const document = config.getDocument(response);
-          // Accumulate comment
-          config.parseForItems(document, acc, url);
-          // Check for more pages
-          if (recurseForMoreItems) {
-            await config.recurseParseForMoreItems(document, acc, url, inspectPageForItems);
-          }
-          resolve();
-        })
-        .catch(function (error) {
-          // Retry (currently forever!!!) but only subpages
-          if (!recurseForMoreItems) {
-            inspectPageForItems(url, acc, recurseForMoreItems);
-          }
-          // handle error
-          reject(error);
-        });
-    });
-  })
-  .catch(error => {
-    try {
-      console.log(url, error.response.statusText);
-    } catch (e2) {
-      console.log(url, error);
-    }
-  });
-};
 
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
@@ -61,7 +19,7 @@ var items = [];
 
 (async function main() {
   await asyncForEach(config.urlsToRecurse, async url => {
-    await inspectPageForItems(config.baseUrl + url, items);
+    await config.inspectPageForItems(config.baseUrl + url, items);
   });
   const csv = new ObjectsToCsv(items);
   await csv.toDisk('./output/' + config.slug.toLowerCase() + '.csv');
